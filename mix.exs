6f7ba12f0fd5143f78a0d4f67c52f32b9cbd1ac0
Kip Cole <kipcole9@gmail.com>
2026-08-01 08:53:17 +1000

    Doc formatters

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Localize.Lua.MixProject do
     [
       app: :localize_lua,
       version: @version,
-      name: "Localize.Lua",
+      name: "Localize Lua",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -43,8 +43,8 @@ defmodule Localize.Lua.MixProject do
   defp deps do
     [
       {:lua, "~> 1.0"},
-      {:localize, "~> 1.0"},
-      {:ex_doc, "~> 0.34", only: [:dev, :release], runtime: false},
+      {:localize, "~> 1.0-rc"},
+      {:ex_doc, "~> 0.34", only: [:dev], runtime: false},
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev], runtime: false}
     ] ++ maybe_json_polyfill()
